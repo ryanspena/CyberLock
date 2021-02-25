a0586d9011ca23c143ae547b12f5f8db75dc7cd5
Ryan Pena <ryanspena@gmail.com>
2021-02-24 21:28:38 -0600

    Character window confirmations added

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numberals = "0123456789"; 
 let specialCharacters = "!@#$%^&*";
 
+function generatePassword() {
+
+let passwordLength = parseInt (prompt ("How many characters do you want?"));
+console.log("passwordLength" , passwordLength);
+if (isNaN(passwordLength) == true) {
+  alert ("Password length must be provided as a number");
+  return ("Invalid input of password not generated.");
+}
+if (passwordLength < 12 ) {
+  alert ("Password length must be at least 12 characters");
+  return ("Password not generated");
+}
+if (passwordLength > 128 ) {
+  alert ("");
+  return ("");
+}
+count = passwordLength;
+
+let hasLC = confirm ("Do you want to include LowerCase letters?");
+let hasUC = confirm ("Do you want to include UpperCase letters?");
+let hasNM = confirm ("Do you want to include numbers?");
+let hasSP = confirm ("Do you want ti include symbols?");
+
+if (hasLC === false && hasUC === false && hasNM === false && hasSP === false) {
+  alert ("At least one character type needs to be selected");
+  return ("Enter at least one selection.");
+}
+
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
