dd9974390eac5b7d6cd1449f4cc3e7d138987956
Ryan Pena <ryanspena@gmail.com>
2021-02-25 17:54:46 -0600

    removed lines from if statements to create randomization

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ if (isNaN(passwordLength) == true) {
   alert ("Password length must be provided as a number");
   return ("Invalid input of password not generated.");
 }
-if (passwordLength < 12 && passwordLength > 128 ) {
-  alert ("");
+if (passwordLength < 12 || passwordLength > 128 ) {
+  alert ("Incorrect");
   return ("Password not generated");
 }
 count = passwordLength;
@@ -33,30 +33,30 @@ if (hasLC === false && hasUC === false && hasNC === false && hasSP === false) {
 
 if (hasLC === true) {
   possibleCharacters+= lowerCase;
-  generatedPassword+= lowerCase [Math.floor (Math.random() * lowerCase.length)];
-  console.log("Generated password" , generatedPassword);
-  count--;
+  // generatedPassword+= lowerCase [Math.floor (Math.random() * lowerCase.length)];
+  // console.log("Generated password" , generatedPassword);
+  // count--;
 }
 
 if (hasUC === true) {
   possibleCharacters+= upperCase;
-  generatedPassword+= upperCase [Math.floor (Math.random() * upperCase.length)];
-  console.log ("Generated password" , generatedPassword);
-  count--;
+  // generatedPassword+= upperCase [Math.floor (Math.random() * upperCase.length)];
+  // console.log ("Generated password" , generatedPassword);
+  // count--;
 }
 
 if (hasNC === true) {
   possibleCharacters+= numberalCharacters;
-  generatedPassword+= numberalCharacters [Math.floor (Math.random() * numberalCharacters.length)];
-  console.log ("generated password" , generatedPassword);
-  count--;
+  // generatedPassword+= numberalCharacters [Math.floor (Math.random() * numberalCharacters.length)];
+  // console.log ("generated password" , generatedPassword);
+  // count--;
 }
 
 if (hasSP === true) {
   possibleCharacters+= specialCharacters;
-  generatedPassword+= specialCharacters [Math.floor (Math.random() * specialCharacters.length)];
-  console.log ("generated password" , generatedPassword);
-  count--;
+  // generatedPassword+= specialCharacters [Math.floor (Math.random() * specialCharacters.length)];
+  // console.log ("generated password" , generatedPassword);
+  // count--;
 }
 
 for (var i = 0; i < count; i++) {
